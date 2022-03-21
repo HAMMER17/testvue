5452ba2c5ae34de760faa4f6c2637bbe0da85470
HAMMER17 <plutalov70gmail.com>
2022-03-21 22:38:17 +0300

    navbar

---
+++ project-vue/src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+    <NavBar @click="isOpen=!isOpen" />
+    <SideBar v-model="isOpen"/>
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/Navbar.vue'
+import SideBar from '@/components/Sidebar.vue'
+export default {
+  name: 'main-layout',
+  data: () => ({
+    isOpen: true,
+  }),
+  components: {
+    NavBar, SideBar
+  }
+}
+</script>
--- project-vue/src/router/index.js
+++ project-vue/src/router/index.js
@@ -1,25 +1,65 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
+    path: '/registr',
+    name: 'registr',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Login.vue')
   },
-];
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main' },
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detal',
+    name: 'detal',
+    meta: { layout: 'main' },
+    component: () => import('../views/DetalRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main' },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/',
+    name: 'home',
+    meta: { layout: 'main' },
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
