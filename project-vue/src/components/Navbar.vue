0d541e480d2dff32a734e65666a026951de45aa6
HAMMER17 <plutalov70gmail.com>
2022-03-22 19:58:21 +0300

    time

--- project-vue/src/components/Navbar.vue
+++ project-vue/src/components/Navbar.vue
@@ -5,7 +5,7 @@
           <a href="#" @click.prevent="$emit('toggle')">
             <i class="material-icons black-text">dehaze</i>
           </a>
-          <span class="black-text">12.12.12</span>
+          <span class="black-text">{{dateFilter('datetime')}}</span>
         </div>
 
         <ul class="right hide-on-small-and-down">
@@ -14,6 +14,7 @@
               class="dropdown-trigger black-text"
               href="#"
               data-target="dropdown"
+              ref="drop"
             >
               USER NAME
               <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
 
             <ul id="dropdown" class="dropdown-content">
               <li>
-                <a href="#" class="black-text">
+                <router-link to="/profile" class="black-text">
                   <i class="material-icons">account_circle</i>Профиль
-                </a>
+                </router-link>
               </li>
               <li class="divider" tabindex="-1"></li>
               <li>
-                <a href="#" class="black-text">
+                <a href="#" class="black-text" @click.prevent="logout">
                   <i class="material-icons">assignment_return</i>Выйти
                 </a>
               </li>
@@ -37,3 +38,46 @@
       </div>
     </nav>
 </template>
+
+<script>
+export default {
+  data:() => ({
+   date: new Date(),
+   interval: null,
+   drop: null,
+  }),
+  methods: {
+    logout(){
+      this.$router.push('/login')
+    }
+  },
+  mounted() {
+  this.interval = setInterval(() => {
+      this.date = new Date()
+    }, 1000)
+    M.Dropdown.init(this.$refs.drop, {
+      constrainWidth: false
+    })
+  },
+  beforeDestroy() {
+   this.drop = clearInterval(this.interval)
+  },
+  methods: {
+ dateFilter(value, format = 'date') {
+        value = this.date
+        const options= {}
+
+        if (format.includes('date')) {
+          options.day = '2-digit'
+          options.month = 'long'
+          options.year = 'numeric'
+          options.hour = '2-digit'
+          options.minute = '2-digit'
+          options.second = '2-digit'
+        }
+
+        return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
+      }
+}
+}
+</script>
